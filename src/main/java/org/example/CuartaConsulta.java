6240754fdbf38e82752f8475f85ab374a432bc27
jorgeoj <jorge2002oj@gmail.com>
2024-02-06 09:22:39 +0100

    Cuarta y quinta consulta hechas

--- src/main/java/org/example/CuartaConsulta.java
+++ src/main/java/org/example/CuartaConsulta.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URI;
@@ -7,14 +8,28 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * Clase que realiza consultas relacionadas con la película "Dragonball Evolution" y su elenco,
+ * obteniendo información sobre el actor que interpreta el papel de Goku.
+ */
 public class CuartaConsulta {
     // Consulta: Lugar de nacimiento y signo del zodiaco del actor que hacía de Goku en la pelicula "Dragonball Evolution"
+    /**
+     * Método principal que ejecuta las consultas y muestra información sobre el actor que interpreta a Goku.
+     *
+     * @param args Argumentos de la línea de comandos (no utilizados en este caso).
+     */
     public static void main(String[] args) {
-        System.out.println(obtenerPeliculaPorNombre());
+        System.out.println(obtenerDatosActor(obtenerActorGoku(obtenerPeliculaPorNombre())));
     }
 
+    /**
+     * Realiza una consulta para obtener el ID de la película "Dragonball Evolution" por su nombre.
+     *
+     * @return El ID de la película obtenido.
+     */
     public static String obtenerPeliculaPorNombre() {
-        String res = "";
+        String movieId = "";
 
         try {
             HttpRequest request = HttpRequest.newBuilder()
@@ -26,31 +41,92 @@ public class CuartaConsulta {
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             JSONObject jsonResponse = new JSONObject(response.body());
 
-            // TODO esto da error no se porque
-            String movieId = jsonResponse.getJSONObject("results").getString("imdb_id");
-            System.out.println(movieId);
+            JSONArray resultsArray = jsonResponse.getJSONArray("results");
+            if (resultsArray.length() > 0) {
+                JSONObject movieInfo = resultsArray.getJSONObject(0);
+                movieId = movieInfo.getString("imdb_id");
 
-            res = movieId;
+                System.out.println("id de la película: " + movieId);
+            } else {
+                System.out.println("No se encontraron resultados para la película.");
+            }
 
-            System.out.println(jsonResponse.toString(2));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return res;
+        return movieId;
     }
 
-    /*
-    retrievegetMovieIdByTitle ->
-    imdb_id:"tt1098327"
-    title:"Dragonball Evolution"
+    /**
+     * Realiza una consulta para obtener el ID del actor que interpreta a Goku en la película especificada por su ID.
+     *
+     * @param idPelicula El ID de la película.
+     * @return El ID del actor que interpreta a Goku.
+     */
+    public static String obtenerActorGoku(String idPelicula) {
+        String actor = "";
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://moviesminidatabase.p.rapidapi.com/movie/id/"+ idPelicula +"/cast/"))
+                    .header("X-RapidAPI-Key", APIConfig.API_KEY)
+                    .header("X-RapidAPI-Host", "moviesminidatabase.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject jsonResponse = new JSONObject(response.body());
 
-    listgetCastByMovieids (con el id que sacamos antes) -> donde el "role" = "Goku"
-    imdb_id:"nm0154226"
-    name:"Justin Chatwin"
+            if (jsonResponse.has("results")) {
+                JSONArray rolesArray = jsonResponse.getJSONObject("results").getJSONArray("roles");
 
-    retrievegetActorDetailsById (con el id del paso anterior) ->
-    birth_place:"Nanaimo, British Columbia, Canada"
-    star_sign:"Scorpio"
-    */
+                // Buscar el actor que interpreta el papel de Goku
+                for (int i = 0; i < rolesArray.length(); i++) {
+                    JSONObject role = rolesArray.getJSONObject(i);
 
+                    if ("Goku".equalsIgnoreCase(role.getString("role"))) {
+                        actor = role.getJSONObject("actor").getString("imdb_id");
+                        System.out.println("id del actor de goku: " + actor);
+                        break;  // Terminar la búsqueda una vez que se encuentra a Goku
+                    }
+                }
+            } else {
+                System.out.println("La respuesta no contiene resultados.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return actor;
+    }
+
+    /**
+     * Realiza una consulta para obtener información sobre el actor especificado por su ID.
+     *
+     * @param idActor El ID del actor.
+     * @return Un objeto JSON con información sobre el actor.
+     */
+    public static JSONObject obtenerDatosActor(String idActor) {
+        JSONObject resultObject = new JSONObject();
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://moviesminidatabase.p.rapidapi.com/actor/id/" + idActor + "/"))
+                    .header("X-RapidAPI-Key", APIConfig.API_KEY)
+                    .header("X-RapidAPI-Host", "moviesminidatabase.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject jsonResponse = new JSONObject(response.body());
+
+            String lugarNacimiento = jsonResponse.getJSONObject("results").getString("birth_place");
+            String cumple = jsonResponse.getJSONObject("results").getString("birth_date");
+            String signo = jsonResponse.getJSONObject("results").getString("star_sign");
+
+            System.out.println("lugar de nacimiento: " + lugarNacimiento);
+            System.out.println("fecha de nacimiento: " + cumple);
+            System.out.println("signo del zodiaco: " +  signo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultObject;
+    }
 }
+
+// System.out.println(jsonResponse.toString(2)); <- Devuelve siempre el resultado completo de la consulta
--- src/main/java/org/example/QuintaConsulta.java
+++ src/main/java/org/example/QuintaConsulta.java
@@ -1,6 +1,42 @@
 package org.example;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ * Clase que realiza una consulta para obtener la cantidad de películas registradas en la base de datos
+ * para el año en que nació un actor específico.
+ */
 public class QuintaConsulta {
     // Consulta: Del año en que nació el actor anterior, ¿Cuántas películas hay registradas en la base de datos?
-    public static void main(String[] args) {}
+    /**
+     * Método principal que realiza la consulta y muestra la cantidad de películas registradas en el año especificado.
+     *
+     * @param args Argumentos de la línea de comandos (no utilizados en este caso).
+     * @throws IOException            Si ocurre un error de entrada/salida durante la ejecución de la solicitud HTTP.
+     * @throws InterruptedException   Si la ejecución es interrumpida durante la espera de la respuesta HTTP.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // Realizar una solicitud HTTP para obtener la información sobre las películas registradas en un año específico
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://moviesminidatabase.p.rapidapi.com/movie/byYear/1982/"))
+                .header("X-RapidAPI-Key", APIConfig.API_KEY)
+                .header("X-RapidAPI-Host", "moviesminidatabase.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Convertir la respuesta JSON a un objeto JSONObject
+        JSONObject objetoJson = new JSONObject(response.body());
+
+        // Obtener la cantidad de objetos en la respuesta
+        int cantidadDePeliculas = objetoJson.getJSONArray("results").length();
+
+        System.out.println("Cantidad de películas registradas en el año 1982: " + cantidadDePeliculas);
+    }
 }
